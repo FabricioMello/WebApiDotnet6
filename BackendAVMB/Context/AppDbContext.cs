2d82293a2fb774cbfa8409675adde56aa4aff434
Fabrício Mello <fabricio_mello_fialho@hotmail.com>
2022-12-01 17:34:45 -0300

    db context

---
+++ BackendAVMB/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BackendAVMB.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendAVMB.Context
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ResponseGetDadosEnvelope.Response> Envelopes { get; set; }
+    }
+}
